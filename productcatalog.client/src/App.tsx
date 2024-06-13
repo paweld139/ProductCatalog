f66c61be29e1fe21bc8ef4d531196e2e543c8c5c
Paweł Dywan <paweldywan@outlook.com>
2024-06-13 13:37:07 +0200

    Clean

--- productcatalog.client/src/App.tsx
+++ productcatalog.client/src/App.tsx
@@ -29,7 +29,7 @@ function App() {
         }, [] as typeof AppRoutes);
 
     useEffect(() => {
-        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
     }, []);
 
     return (
